37db5d6ddbcf25d3f5aaef8889b1e8e0ab695ee1
Deepali Kulshrestha <87146622+Kulshre142@users.noreply.github.com>
2021-12-29 23:22:18 +0530

    Add files via upload

---
+++ String_Imm.java
@@ -0,0 +1,10 @@
+public class String_Imm
+{
+public static void main(String[] args)
+{
+String s1 = "ITM";
+System.out.println(s1);         
+s1.concat("GOI");
+System.out.println(s1);         
+}
+}
