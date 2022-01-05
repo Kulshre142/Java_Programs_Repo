346f3d5b80e2f8502ace64c813437d3f9ee43061
Deepali Kulshrestha <87146622+Kulshre142@users.noreply.github.com>
2022-01-06 00:32:09 +0530

    Add files via upload

---
+++ MinMaxValueWrapper.java
@@ -0,0 +1,16 @@
+public class MinMaxValueWrapper {
+    public static void main(String[] args) {
+        System.out.println("Byte min=" + Byte.MIN_VALUE);
+        System.out.println("Byte max = " + Byte.MAX_VALUE);
+        System.out.println("Short min = " + Short.MIN_VALUE);
+        System.out.println("Short max = " + Short.MAX_VALUE);
+        System.out.println("Integer min = " + Integer.MIN_VALUE);
+        System.out.println("Integer max = " + Integer.MAX_VALUE);
+        System.out.println("Long min = " + Long.MIN_VALUE);
+        System.out.println("Long max = " + Long.MAX_VALUE);
+        System.out.println("Float min = " + Float.MIN_VALUE);
+        System.out.println("Float max = " + Float.MAX_VALUE);
+        System.out.println("Double min = " + Double.MIN_VALUE);
+        System.out.println("Double max = " + Double.MAX_VALUE);
+    }
+}
