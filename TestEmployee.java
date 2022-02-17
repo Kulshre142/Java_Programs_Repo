b87291f868b488b154f1bcf2ae26afd1ff5b96ce
Deepali Kulshrestha <87146622+Kulshre142@users.noreply.github.com>
2022-02-17 23:57:41 +0530

    Add files via upload

---
+++ TestEmployee.java
@@ -0,0 +1,8 @@
+public class TestEmployee{
+	public static void main( String args[]){
+	Person p=new Person("Somya");
+	Employee e=new Employee("Nikhil",25000,2021,"01AC");
+	System.out.println(p);
+	System.out.println(e);
+	}
+}
