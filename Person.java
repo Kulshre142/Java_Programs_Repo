7f2826392cb22895ad29072f56c7ac75952edc76
Deepali Kulshrestha <87146622+Kulshre142@users.noreply.github.com>
2022-02-25 22:50:26 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person {
+private String name;
+	public Person(String n){
+		name=n;
+	}
+	public void setName(String n){
+		name=n;
+	}
+	public String getname(){
+		return name;
+	}
+	public String toStriing(){
+		return name;
+	}
+}
